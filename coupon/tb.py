fcff4d1ffdbf07337b5c65ef4f2ca95b2a51aa09
haohanlin <985339316@qq.com>
2020-08-20 00:53:41 +0800

    测试修改淘宝api获取的url

--- coupon/tb.py
+++ coupon/tb.py
@@ -12,50 +12,59 @@ def tb_share_text(group_name: str, material_id: str, app_key, app_secret, adzone
     :param material_id:
     :return:
     '''
+    print("开始获取-----")
     try:
         material_id = str(random.choices(material_id.split(','))[0])
-        print(material_id)
-        groups = itchat.search_chatrooms(name=f'''{group_name}''')
-        for room in groups:
-            group_name = room['UserName']
-            time.sleep(random.randint(1, 5))
-            tb_client = TbApiClient(app_key=app_key, secret_key=app_secret, adzone_id=adzone_id)
-            res = tb_client.taobao_tbk_dg_optimus_material(material_id)
-            json_data = json.loads(res)['tbk_dg_optimus_material_response']['result_list']['map_data']
-            count = 0
-            for item in json_data:
-                count += 1
-                if str(item).find("coupon_share_url") > -1:
-                    coupon_share_url = "https:" + item['coupon_share_url']
-                    coupon_amount = item['coupon_amount']
-                    pict_url = "https:" + str(item['pict_url'])
-                    title = item['title']
-                    item_id = item['item_id']
-                    filename = save_pic(pict_url, item_id)
-                    zk_final_price = item['zk_final_price']
-                    # 发送图片
-                    itchat.send('@img@%s' % (f'''{filename}'''), group_name)
+        
+        print(material_id+ "whx1")
+        print(group_name + "whx2")
+        #groups = itchat.search_chatrooms(name=f'''{group_name}''')
+        #for room in groups:
+       #group_name = room['UserName']
+        time.sleep(random.randint(1, 5))
+        tb_client = TbApiClient(app_key=app_key, secret_key=app_secret, adzone_id=adzone_id)
+        res = tb_client.taobao_tbk_dg_optimus_material(material_id)
+        print("输出tb_client数据res:----------------------- whx3")
+        print(repr)
+        json_data = json.loads(res)['tbk_dg_optimus_material_response']['result_list']['map_data']
+        count = 0
+        print("输出json数据:----------------------- whx4")
+        #print(json_data)
+        for item in json_data:
+            #print("输出json数据:")
+            #print(json_data)
+            count += 1
+            if str(item).find("coupon_share_url") > -1:
+                coupon_share_url = "https:" + item['coupon_share_url']
+                coupon_amount = item['coupon_amount']
+                pict_url = "https:" + str(item['pict_url'])
+                title = item['title']
+                item_id = item['item_id']
+                filename = save_pic(pict_url, item_id)
+                zk_final_price = item['zk_final_price']
+                # 发送图片
+                itchat.send('@img@%s' % (f'''{filename}'''), group_name)
 
-                    itchat.send(f''' {title} \n【在售价】¥{zk_final_price}\n【券后价】¥{round(float(zk_final_price) - float(coupon_amount),
-                                                                             2)}\n-----------------\n復製評论({tb_client.taobao_tbk_tpwd_create(
-                        title, coupon_share_url)})，去【tao寶】下单\n''', group_name)
+                itchat.send(f''' {title} \n【在售价】¥{zk_final_price}\n【券后价】¥{round(float(zk_final_price) - float(coupon_amount),
+                                                                            2)}\n-----------------\n復製評论({tb_client.taobao_tbk_tpwd_create(
+                    title, coupon_share_url)})，去【tao寶】下单\n''', group_name)
 
-                    time.sleep(2)
-                    del_pic(filename)
-                else:
-                    click_url = "https:" + item['click_url']
-                    title = item['title']
-                    item_id = item['item_id']
-                    pict_url = "https:" + str(item['pict_url'])
-                    zk_final_price = item['zk_final_price']
-                    print(pict_url)
-                    filename = save_pic(pict_url, item_id)
-                    itchat.send('@img@%s' % (f'''{filename}'''), group_name)
-                    itchat.send(
-                        f'''{title} \n【在售价】¥{zk_final_price}\n-----------------\n復製評论({tb_client.taobao_tbk_tpwd_create(
-                            title, click_url)})，去【tao寶】下单\n''', group_name)
-                    time.sleep(2)
-                    del_pic(filename)
+                time.sleep(2)
+                del_pic(filename)
+            else:
+                click_url = "https:" + item['click_url']
+                title = item['title']
+                item_id = item['item_id']
+                pict_url = "https:" + str(item['pict_url'])
+                zk_final_price = item['zk_final_price']
+                print(pict_url)
+                filename = save_pic(pict_url, item_id)
+                itchat.send('@img@%s' % (f'''{filename}'''), group_name)
+                itchat.send(
+                    f'''{title} \n【在售价】¥{zk_final_price}\n-----------------\n復製評论({tb_client.taobao_tbk_tpwd_create(
+                        title, click_url)})，去【tao寶】下单\n''', group_name)
+                time.sleep(2)
+                del_pic(filename)
     except Exception as e:
         print(e)
         tb_share_text(group_name, material_id, app_key, app_secret, adzone_id)
--- untils/scheduler.py
+++ untils/scheduler.py
@@ -37,10 +37,19 @@ def tb_job_tasks(scheduler):
     chat_groups = conf.get('chat_groups')
     for chat_group in chat_groups:
         print(chat_group['group_name'])
+        tb_share_text(group_name=chat_group['group_name'], material_id=chat_group['group_material_id'], app_key=app_key, app_secret=app_secret, adzone_id=adzone_id )
+        """
+        scheduler.add_job(func=tb_share_text,
+                          kwargs={'group_name': chat_group['group_name'], 'material_id': chat_group['group_material_id'],
+                                  'app_key': app_key, 'app_secret': app_secret, 'adzone_id': adzone_id},
+                          trigger='cron', hour=23, minute=24, second=0,  jitter=300, id=f'''{chat_group['group_name']}''')
+        """
+        """ 
         scheduler.add_job(func=tb_share_text,
                           kwargs={'group_name': chat_group['group_name'], 'material_id': chat_group['group_material_id'],
                                   'app_key': app_key, 'app_secret': app_secret, 'adzone_id': adzone_id},
                           trigger='cron', hour=f'''{chat_group['hour']}''', minute=f'''{chat_group['minute']}''', second=0,  jitter=300, id=f'''{chat_group['group_name']}''')
+        """ 
 def jd_job_task(scheduler):
 
     conf = config.get_yaml()
